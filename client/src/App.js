981b945d9e6a31c1e262731b04a4e47b06655c03
EricB <ericblancas23@gmail.com>
2018-11-26 00:12:03 -0800

    modified git ignore

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import Launches  from './components/launches';
+import './App.css';
+import logo from './logo.png';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+      <div className="container">
+        <img src={logo} alt="space x" style={{ width: 300, display: 'block', margin: 'auto'}} />
+       <Launches />
+      </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
