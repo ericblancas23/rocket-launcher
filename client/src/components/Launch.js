acebd3eaeef652f9d0b6102953caf59510929046
EricB <ericblancas23@gmail.com>
2018-11-29 21:48:23 -0800

    working param

--- client/src/components/Launch.js
+++ client/src/components/Launch.js
@@ -4,49 +4,52 @@ import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
 import classNames from 'classnames'
 
-// const LAUNCH_QUERY = gql`
-//     query LaunchQuery($flight_number: Int!) {
-//         launch(flight_number: $flight_number) {
-//            flight_number
-//            mission_name
-//            launch_year
-//            launch_success
-//            launch_date_local
-//            rocket {
-//                rocket_id
-//                rocket_name
-//                rocket_type
-//            }
-//         }
-//     }
-// `;
+const LAUNCH_QUERY = gql`
+    query LaunchQuery($flight_number: Int!) {
+        launch(flight_number: $flight_number) {
+           flight_number
+           mission_name
+           launch_year
+           launch_success
+           launch_date_local
+           Rocket {
+               rocket_id
+               rocket_name
+               rocket_type
+           }
+        }
+    }
+`;
 
 
 export class Launch extends Component {
   render() {
-    // let { flight_number } = this.props.params;
-    // flight_number = parseInt(flight_number);
+    let { flight_number } = this.props.match.params;
+    flight_number = parseInt(flight_number);
     return (
-      <div>
-        {/* <Query query={LAUNCH_QUERY} variables={{ flight_number}}>
+      <Fragment>
+        <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
             {({ loading, error, data}) => {
-                if(loading) return <h4>...Laoding</h4>;
+                if(loading) return <h4>...Loading</h4>;
                 if (error) console.log(error);
 
-                const {
-                    mission_name,
-                    flight_number,
-                    launch_year,
-                    launch_success,
-                    rocket: {rocket_id, rocket_name, rocket_type}
-                } = data.launch;
+
+                console.log(data);
+                return <div><h1 className="display-4 my-3"><span className="text-dark">Mission:</span>
+                {data.mission_name}
+                </h1>
+                <h4 className="mb-3">Launch Details</h4>
+                <ul className="list-group">
+                  <li className="list-group-item">Flight: {data.flight_number}</li>
+                </ul>
+                </div>
             }}
-        </Query> */}
-        <h1>Launch</h1>
-      </div>
+        </Query>
+      </Fragment>
     )
   }
 }
 
 export default Launch
+
 
